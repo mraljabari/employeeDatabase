d5416254c6b3a9887d1de98c23b864b29176fdbe
mraljabri <hydeki.rizky@gmail.com>
2022-05-03 21:32:15 +0700

    create commit

---
+++ src/employeedatabase/EmployeeDatabase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeedatabase;
+
+/**
+ *
+ * @author M RIZKI AL
+ */
+public class EmployeeDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
